9654933d6a91afa49925979fdec298cade17e2b1
Marek Chrapek <marek.chrapek@hotmail.com>
2020-03-18 18:45:11 +0100

    #29 - Seperate common config

---
+++ common/src/main/java/com/journeyplanner/common/config/paths/Paths.java
@@ -0,0 +1,33 @@
+package com.journeyplanner.common.config.paths;
+
+public class Paths {
+
+    public final static String[] GET_PERMIT_ALL_PATHS = {
+
+    };
+
+    public final static String[] POST_PERMIT_ALL_PATHS = {
+            "/user/register",
+            "/user/reset"
+    };
+
+    public final static String[] GET_USER_PATHS = {
+            "/user/info"
+    };
+
+    public final static String[] POST_USER_PATHS = {
+
+    };
+
+    public final static String[] GET_ADMIN_PATHS = {
+            "/user"
+    };
+
+    public final static String[] POST_ADMIN_PATHS = {
+            "/user/block"
+    };
+
+    public final static String[] DELETE_ADMIN_PATHS = {
+            "/user/block"
+    };
+}
