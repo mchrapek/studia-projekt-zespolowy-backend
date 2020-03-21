3b175c7c11ba56361cfc95eaec8222e29e644c18
Marek Chrapek <marek.chrapek@hotmail.com>
2020-03-21 21:28:53 +0100

    #64 - Create payment service

---
+++ payment-service/src/main/java/com/journeyplanner/payment/app/PaymentServiceApplication.java
@@ -0,0 +1,24 @@
+package com.journeyplanner.payment.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {
+        "com.journeyplanner.payment.config",
+        "com.journeyplanner.payment.domain",
+        "com.journeyplanner.payment.infrastructure"
+})
+@EnableEurekaClient
+@EnableMongoRepositories(basePackages = {
+        "com.journeyplanner.payment.domain"
+})
+public class PaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentServiceApplication.class, args);
+    }
+}
