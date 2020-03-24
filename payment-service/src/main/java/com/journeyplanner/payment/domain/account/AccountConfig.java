7ad8f05cc321e438e678bc161ece3deae6aa396f
Marek Chrapek <marek.chrapek@hotmail.com>
2020-03-24 21:54:51 +0100

    #86 - Payment emails

--- payment-service/src/main/java/com/journeyplanner/payment/domain/account/AccountConfig.java
+++ payment-service/src/main/java/com/journeyplanner/payment/domain/account/AccountConfig.java
@@ -1,5 +1,6 @@
 package com.journeyplanner.payment.domain.account;
 
+import com.journeyplanner.payment.infrastructure.output.MailSender;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 public class AccountConfig {
 
     @Bean
-    AccountFacade accountFacade(AccountRepository accountRepository, AccountHistoryRepository accountHistoryRepository, TransferRepository transferRepository) {
-        return new AccountFacade(accountRepository, new AccountCreator(), accountHistoryRepository, transferRepository, new AccountHistoryCreator());
+    AccountFacade accountFacade(AccountRepository accountRepository, AccountHistoryRepository accountHistoryRepository, TransferRepository transferRepository, MailSender mailSender) {
+        return new AccountFacade(accountRepository, new AccountCreator(), accountHistoryRepository, transferRepository, new AccountHistoryCreator(), mailSender);
     }
 }
---
+++ payment-service/src/main/java/com/journeyplanner/payment/infrastructure/output/MailSender.java
@@ -0,0 +1,35 @@
+package com.journeyplanner.payment.infrastructure.output;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.journeyplanner.common.config.events.Event;
+import com.journeyplanner.common.config.events.EventPublisher;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import static java.text.MessageFormat.format;
+
+@Service
+@Slf4j
+public class MailSender implements EventPublisher {
+
+    private final String queueName;
+    private final RabbitTemplate rabbitTemplate;
+
+    public MailSender(@Value("${queue.mail.name}") String queueName, RabbitTemplate rabbitTemplate) {
+        this.queueName = queueName;
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Override
+    public void publish(Event event) {
+        try {
+            String mappedEvent = new ObjectMapper().writeValueAsString(event);
+            rabbitTemplate.convertAndSend(queueName, mappedEvent);
+            log.info(format("Event send : {0} : {1}", queueName, mappedEvent));
+        } catch (Exception e) {
+            log.error(format("Cannot send message to : {0} : {1}", queueName, e.getMessage()));
+        }
+    }
+}
