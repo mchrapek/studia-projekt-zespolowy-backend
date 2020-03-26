52b0066b5dcd165a57c4374973f03198f2f78244
Marek Chrapek <marek.chrapek@hotmail.com>
2020-03-26 14:02:56 +0100

    #102 - Translate test data

--- catalogue-service/src/main/java/com/journeyplanner/catalogue/domain/journey/JourneyRepositoryConfiguration.java
+++ catalogue-service/src/main/java/com/journeyplanner/catalogue/domain/journey/JourneyRepositoryConfiguration.java
@@ -16,21 +16,10 @@ public class JourneyRepositoryConfiguration {
         Journey journey = Journey.builder()
                 .id("57ee7379-eaa9-4029-9267-dd0eb6a51b2b")
                 .status(JourneyStatus.ACTIVE)
-                .name("Jarmark Bożonarodzeniowy Lipsk Express")
-                .country("Niemcy")
-                .city("Lipsk")
-                .description("Dzień 1 Wyjazd wg rozkładu jazdy (wyjazd w nocy z piątku na sobotę). Przejazd do Lipska. Zwiedzanie - " +
-                        "słynny Kościół św. Mikołaja, galeria handlowa Specks Hof, mały targ przekąsek, rynek ze Starym Ratuszem, Mädler Passage z " +
-                        "Piwnicą Auerbacha, Barthels Hof i Kościół św. Tomasza. Ekskluzywna dzielnica Gohlis - zamek Gohlis, zbudowany w połowie " +
-                        "XVIII wieku jako letnia rezydencja burżuazji. Dom Schillera - skromny domek wiejski, w którym Friedrich Schiller mieszkał " +
-                        "przez kilka miesięcy w 1785 roku pisząc \"Odę do Radości\". Nieustająco zmieniająca się Plagwitz z kanałami i galeriami. " +
-                        "Ta muzyczna dzielnica zachwyca zielenią i imponującymi willami. Od 2002 roku w budynku byłego Trybunału Ludowego działa " +
-                        "Federalny Sąd Administracyjny. Trasa wycieczki prowadzi przez tętniące życiem południowe przedmieścia i obok popularnego " +
-                        "Panometeru do słynnego miejsca w Lipsku, czyli Pomnika Bitwy Narodów. Następnie udamy się przez Stare Miasto, Plac niemiecki " +
-                        "i obok bawarskiej stacji kolejowej do Augustusplatz. Tu znajdują się trzy ważne instytucje kulturowe, które w znaczący sposób " +
-                        "kształtują wizerunek miasta - nowa sala koncertowa, uniwersytet i opera. Pobyt na kolorowych, pięknie przystrojonych jarmarkach " +
-                        "adwentowych, które odbywają się na terenie całego historycznego centrum miasta. Wyjazd w drogę powrotną w godzinach wieczornych. " +
-                        "Dzień 2 Powrót do Polski w godzinach porannych.")
+                .name("Beautiful Leipzig and Dresden")
+                .country("Germany")
+                .city("Leipzig and Dresden")
+                .description("Day 1: Visiting beautiful Leipzig; Day 2: Visiting beautiful Dresden")
                 .transportType("Train")
                 .price(new BigDecimal(1000L))
                 .start(Instant.now().plus(100, ChronoUnit.DAYS))
