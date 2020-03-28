95ce862ea0c7c93369c2de63f087d22401a533f2
Marek Chrapek <marek.chrapek@hotmail.com>
2020-03-28 10:48:44 +0100

    #8 - Test step ci

--- reservation-service/src/test/groovy/com/journeyplanner/reservation/domain/reservation/CreateReservationSpec.groovy
+++ reservation-service/src/test/groovy/com/journeyplanner/reservation/domain/reservation/CreateReservationSpec.groovy
@@ -1,12 +1,14 @@
 package com.journeyplanner.reservation.domain.reservation
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.annotation.DirtiesContext
 import spock.lang.Specification
 
 @SpringBootTest
 @DirtiesContext
+@AutoConfigureMockMvc
 class CreateReservationSpec extends Specification {
 
     @Autowired
